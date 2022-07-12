f196a3369794aa69c14e4472074e00e5f35940a8
Manuel-Antunes <nerdbroz2@gmail.com>
2022-07-12 00:00:44 -0300

    feat: implement a bare app routes

---
+++ src/routes/app.routes/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import App from '../../pages/App';
+
+const AppRouter = createNativeStackNavigator();
+
+const AppRoutes = () => {
+  return (
+    <AppRouter.Navigator>
+      <AppRouter.Screen name="App" component={App} />
+    </AppRouter.Navigator>
+  );
+};
+
+export default AppRoutes;
